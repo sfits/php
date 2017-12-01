1b29fba66290139889ee0ed667b9ccf74da6bdb0
sfits <avrock@yndex.ru>
2017-12-02 01:26:45 +0200

    Gallery: cheking files extentions, new HEADER

--- function.php
+++ function.php
@@ -1,16 +1,21 @@
 <?php
-// загрузка файлов на сервер
-$uploaddir = __DIR__ . '/img/';
-$uploadfile = $uploaddir . basename($_FILES['img']['name']); 
-if (move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile)) {
-	header("Location: index.php");
-	echo 'Downloaded <br> <a href="index.php"><= Go back</a>';
-}
 
+// вывод картинки по клику
 if ($_GET['file']) {
 	echo '<img src="img/' . $_GET['file'] . '"></a>';
+} else {
+	header("Location: index.php");
 }
 
-$newFile = basename($_FILES['img']['name']);
-$imgs [] = $newFile;
+// проверка расширения файла
+$str = $_FILES['img']['name'];
+$dot = substr(strstr($str, '.'), 1, strlen($str));
 
+// загрузка файлов на сервер
+$uploaddir = __DIR__ . '/img/';
+if (($dot == 'jpg') or ($dot == 'jpeg') or ($dot == 'bmp')) {
+	$uploadfile = $uploaddir . basename($_FILES['img']['name']);
+	if (move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile)) {
+		header("Location: index.php");
+	}
+}
