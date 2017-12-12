d9ac388183412d0e2630c867d7f173e60d27df5f
sfits <avrock@yandex.ru>
2017-12-12 23:47:35 +0200

    Update startup.php

--- startup.php
+++ startup.php
@@ -2,21 +2,21 @@
 
 function startup()
 {
-	// Настройки подключения к БД.
+	// РќР°СЃС‚СЂРѕР№РєРё РїРѕРґРєР»СЋС‡РµРЅРёСЏ Рє Р‘Р”.
 	$hostname = 'localhost'; 
 	$username = 'root'; 
 	$password = '';
 	$dbName = 'localhost';
 	
-	// Языковая настройка.
+	// РЇР·С‹РєРѕРІР°СЏ РЅР°СЃС‚СЂРѕР№РєР°.
 	setlocale(LC_ALL, 'ru_RU.CP1251');	
 	
-	// Подключение к БД.
+	// РџРѕРґРєР»СЋС‡РµРЅРёРµ Рє Р‘Р”.
 	mysql_connect($hostname, $username, $password) or die('No connect with data base'); 
 	mysql_query('SET NAMES cp1251');
 	mysql_select_db($dbName) or die('No data base');
 
-	// Открытие сессии.
+	// РћС‚РєСЂС‹С‚РёРµ СЃРµСЃСЃРёРё.
 	session_start();
 		
 }
