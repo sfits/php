a1ff79e7aef2bda12135b0f671e0d1efa80958bf
sfits <avrock@yandex.ru>
2017-12-12 23:52:52 +0200

    Update editor.php

--- theme/editor.php
+++ theme/editor.php
@@ -1,1 +1,40 @@
-<?/*Шаблон редактируемой страницы=======================$articles - список статейстатья:id_article - идентификаторtitle - заголвокcontent - текст*/?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head>	<title>Blog</title>	<meta content="text/html; charset=Windows-1251" http-equiv="content-type">		<link rel="stylesheet" type="text/css" media="screen" href="theme/style.css" /> </head><body>	<h1>Blog</h1>	<br/>	<a href="index.php">Главная</a> |	<b>Консоль редактора</b>	<hr/>	<ul>		<li>			<b><a href="new.php">Новая статья</a></b>		</li>			<? foreach ($articles as $article): ?>			<li>				<a href="edit.php?id=<?=$article['id_article']?>">					<?=$article['title']?>				</a>			</li>		<? endforeach ?>	</ul>	<hr/>	<small><a href="">Copyright</a> &copy;</small>			</body></html>
+<?/*
+РЁР°Р±Р»РѕРЅ СЂРµРґР°РєС‚РёСЂСѓРµРјРѕР№ СЃС‚СЂР°РЅРёС†С‹
+=======================
+$articles - СЃРїРёСЃРѕРє СЃС‚Р°С‚РµР№
+
+СЃС‚Р°С‚СЊСЏ:
+id_article - РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂ
+title - Р·Р°РіРѕР»РІРѕРє
+content - С‚РµРєСЃС‚
+*/?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<title>Blog</title>
+	<meta content="text/html; charset=Windows-1251" http-equiv="content-type">	
+	<link rel="stylesheet" type="text/css" media="screen" href="theme/style.css" /> 
+</head>
+<body>
+	<h1>Blog</h1>
+	<br/>
+	<a href="index.php">Р“Р»Р°РІРЅР°СЏ</a> |
+	<b>РљРѕРЅСЃРѕР»СЊ СЂРµРґР°РєС‚РѕСЂР°</b>
+	<hr/>
+	<ul>
+		<li>
+			<b><a href="new.php">РќРѕРІР°СЏ СЃС‚Р°С‚СЊСЏ</a></b>
+		</li>	
+		<? foreach ($articles as $article): ?>
+			<li>
+				<a href="edit.php?id=<?=$article['id_article']?>">
+					<?=$article['title']?>
+				</a>
+			</li>
+		<? endforeach ?>
+	</ul>
+	<hr/>
+	<small><a href="">Copyright</a> &copy;</small>			
+</body>
+</html>
